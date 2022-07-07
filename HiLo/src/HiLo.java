e7d0e5eeb0941eb9d238e57c6b47f1937e1fa5ad
Kelly <Kelly@Tisiphone.lan1>
2022-07-07 17:47:03 -0400

    Test HiLo commit from Eclipse

---
+++ HiLo/src/HiLo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String playAgain = "";
+		do {
+			// create a random number for user to guess
+			// (int) is a type cast -> changes type from decimal to int
+			int theNumber = (int)(Math.random() * 200 - 100);
+			// System.out.println(theNumber);
+			int guess = 0;
+			int numOfTries = 0;
+			while (guess != theNumber) {
+				System.out.println("Guess a number between -100 and 100: ");
+				// nextInt() takes the string of text chars entered by user and
+				// turns it into the correct numeric value
+				guess = scan.nextInt();
+				numOfTries = numOfTries += 1;
+				if (guess < theNumber) {
+					System.out.println(guess + " is too low. Try again!");
+				}
+				else if (guess > theNumber) {
+					System.out.println(guess + " is too high. Try again!");
+				}
+				else
+					System.out.println(guess + " is correct. You win!");
+			}
+			// end of while loop
+			System.out.println("It took you " + numOfTries + " tries. Nice!");
+			System.out.println("Would you like to play again? (Y/N)");
+			playAgain = scan.next();
+		} while (playAgain.equalsIgnoreCase("Y"));
+		System.out.println("Thanks for playing! Goodbye.");
+		scan.close();
+	}
+
+}	
